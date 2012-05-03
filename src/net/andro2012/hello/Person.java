b19b305bc6960f8c1572ab3b5030c234564e49d3
kenu <kenu.heo@gmail.com>
2012-05-03 21:49:51 +0900

    init

---
+++ src/net/andro2012/hello/Person.java
@@ -0,0 +1,38 @@
+package net.andro2012.hello;
+
+public class Person {
+	private String name;
+	private int age;
+	private String email;
+	private String address;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + ", email=" + email
+				+ ", address=" + address + "]";
+	}
+	
+}
